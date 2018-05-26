75c3166736c4a6f0c33870fc0ae25ab5cebc5908
ketutadji <ikilampan97@gmail.com>
2018-05-26 12:14:12 +0700

    pencarian

--- application/controllers/Tes.php
+++ application/controllers/Tes.php
@@ -6,11 +6,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 class Tes extends CI_Controller
 {
-	    public function index(){
-      
+	
+
+	public function __construct() {
+        parent::__construct();
+        $this->load->model('M_data'); //memanggil model
+    }
+
+    public function index(){
+      $data['penduduk'] = $this->M_data->tampil(); //memanggil function tampil di model m_data
+     $this->load->view('header');
 		$this->load->view('navbar');
 		$this->load->view('sidebar');
-		$this->load->view('content');		
+		$this->load->view('content', $data);		
 		$this->load->view('footer');
     }
 
